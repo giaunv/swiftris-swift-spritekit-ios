0df79e500de0a118fb25439c5b46c5b8d4b45cf1
Giau <giaunv@gmail.com>
2015-02-28 16:14:30 +0700

    Shaping Up

---
+++ swiftris/Shape.swift
@@ -0,0 +1,126 @@
+//
+//  Shape.swift
+//  swiftris
+//
+//  Created by giaunv on 2/28/15.
+//  Copyright (c) 2015 366. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+let NumOrientations: UInt32 = 4
+
+enum Orientation: Int, Printable{
+    case Zero = 0, Ninety, OneEighty, TwoSeventy
+    
+    var description: String{
+        switch self{
+        case .Zero:
+            return "0"
+        case .Ninety:
+            return "90"
+        case .OneEighty:
+            return "180"
+        case .TwoSeventy:
+            return "270"
+        }
+    }
+    
+    static func random() -> Orientation{
+        return Orientation(rawValue: Int(arc4random_uniform(NumOrientations)))!
+    }
+    
+    static func rotate(orientaton: Orientation, clockwise: Bool) -> Orientation{
+        var rotated = orientaton.rawValue + (clockwise ? 1 : -1)
+        if(rotated > Orientation.TwoSeventy.rawValue){
+            rotated = Orientation.Zero.rawValue
+        }
+        else if(rotated < 0){
+            rotated = Orientation.TwoSeventy.rawValue
+        }
+        
+        return Orientation(rawValue: rotated)!
+    }
+}
+
+// The number of total shape varieties
+let NumShapeTypes: UInt32 = 7
+
+// Shape indexes
+let FirstBlockIdx: Int = 0
+let SecondBlockIdx: Int = 1
+let ThirdBlockIdx: Int = 2
+let FourthBlockIdx: Int = 3
+
+class Shape: Hashable, Printable {
+    // The color of the shape
+    let color: BlockColor
+    
+    // The blocks comprising the shape
+    var blocks = Array<Block>()
+    
+    // The current orientation of the shape
+    var orientation: Orientation
+    
+    // The column and row representing the shape's anchor point
+    var column, row: Int
+    
+    //Required overrides
+    
+    //Subclasses must override this property
+    var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>]{
+        return [:]
+    }
+    
+    //Subclasses must override this property
+    var bottomBlocksForOrientations: [Orientation: Array<Block>]{
+        return [:]
+    }
+    
+    var bottomBlocks:Array<Block>{
+        if let bottomBlocks = bottomBlocksForOrientations[orientation]{
+            return bottomBlocks
+        }
+        
+        return []
+    }
+    
+    //Hashable
+    var hashValue: Int{
+        return reduce(blocks, 0){$0.hashValue ^ $1.hashValue}
+    }
+    
+    //Printable
+    var description: String{
+        return "\(color) block facing \(orientation): \(blocks[FirstBlockIdx]), \(blocks[SecondBlockIdx]), \(blocks[ThirdBlockIdx]), \(blocks[FourthBlockIdx])"
+    }
+    
+    init(column: Int, row: Int, color: BlockColor, orientation: Orientation){
+        self.column = column
+        self.row = row
+        self.color = color
+        self.orientation = orientation
+        initializeBlocks()
+    }
+    
+    convenience init(column: Int, row: Int){
+        self.init(column: column, row: row, color: BlockColor.random(), orientation: Orientation.random())
+    }
+    
+    final func initializeBlocks(){
+        if let blockRowColumnTranslations = blockRowColumnPositions[orientation]{
+            for i in 0..<blockRowColumnTranslations.count{
+                let blockRow = row + blockRowColumnTranslations[i].rowDiff
+                let blockColumn = column + blockRowColumnTranslations[i].columnDiff
+                let newBlock = Block(column: blockColumn, row: blockColumn, color: color)
+                blocks.append(newBlock)
+            }
+        }
+    }
+}
+
+func ==(lhs: Shape, rhs: Shape) -> Bool{
+    return lhs.row == rhs.row && lhs.column == rhs.column
+}
+
